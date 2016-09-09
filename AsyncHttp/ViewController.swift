d1422894acbc72bd9c61915e179fc0eb2ed42eae
Michael Teter <michael.teter@gmail.com>
2016-09-09 20:41:43 +0200

    stuff is pretty fucked.

--- AsyncHttp/ViewController.swift
+++ AsyncHttp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var txtUrl:     UITextField!
@@ -14,53 +15,93 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblElapsed: UILabel!
     @IBOutlet weak var lblResult:  UILabel!
 
-    var waiting = false
     var startTime: Date!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    func startClock() {
-        startTime = Date()
-    }
+    let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
+    var dataTask: URLSessionDataTask?
     
-    func elapsedSeconds() -> TimeInterval {
-        return Date().timeIntervalSince(startTime)
-    }
+    func markStartTime() { startTime = Date() }
+    func elapsedSeconds() -> TimeInterval { return Date().timeIntervalSince(startTime) }
+    func updateElapsed() { lblElapsed.text = String(format: "%.2f seconds", elapsedSeconds()) }
     
     @IBAction func btnGoPressed(_ sender: UIButton) {
-        if waiting {
-            // cancel request
+        if isRequestRunning() {
+            cancelRequest()
             lblResult.text = "Request canceled"
-            lblElapsed.text = String(format: "%.2f seconds", elapsedSeconds())
-            waiting = false
         } else {
+            setupRequest()
             lblResult.text = "Request sent... waiting for response"
-            waiting = true
-            startClock()
+            doRequest()
+        }
+    }
+
+    func isRequestRunning() -> Bool {
+        if let task = dataTask {
+            switch task.state {
+            case .running:
+                print("task is .running")
+                return true
+            case .completed:
+                print("task is .completed")
+                return false
+            case .canceling:
+                print("task is .canceling")
+                return false
+            case .suspended:
+                print("task is .suspended")
+                return false
+            }
+        }
+        return false
+    }
+    
+    func cancelRequest() { if dataTask != nil { dataTask?.cancel() } }
+    
+    func setupRequest() {
+        guard txtUrl.text != nil else { return }
+        
+        cancelRequest()
+        
+        let url = URL(string: txtUrl.text!)
+        
+        dataTask = defaultSession.dataTask(with: url!) { data, response, error in
+            DispatchQueue.main.async {
+                //UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+                guard error == nil else {
+                    print(error!.localizedDescription)
+                    return
+                }
+                
+                guard let responseData = data else {
+                    print("Error: did not receive data")
+                    return
+                }
+                
+                do {
+                    guard let json = try NSJSONSerialization.JSONObjectWithData(responseData, options: []) as? [String: Any] else {
+                        print("Error trying to convert data to json")
+                        return
+                    }
+                    
+                    print(json.description)
+                    self.lblResult.text = json.description
+                }
+            }
         }
     }
 
     func doRequest() {
-        if let url = URL(string: txtUrl.text!), txtUrl.text != nil {
-            let req = NSMutableURLRequest(url: url)
-            var session = URLSession.shared
-            req.httpMethod = "GET"
-
-//            req.httpBody = try? JSONSerialization.data(withJSONObject: <#T##Any#>, options: <#T##JSONSerialization.WritingOptions#>)
-//            req.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(params, options: [])
-            
-            req.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            req.addValue("application/json", forHTTPHeaderField: "Accept")
-            
-            var task = session.dataTask(with: url, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
-            var task = session.dataTaskWithRequest(req, completionHandler: {data, response, error -> Void in
-                print("Response: \(response)")})
-            
+        if let task = dataTask {
+            lblElapsed.text = "... running ..."
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            markStartTime()
             task.resume()
-            
+        }
+
+        while isRequestRunning() {
+            updateElapsed()
+        }
     }
 }
 
